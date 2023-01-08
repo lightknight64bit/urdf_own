a5a1364285783cdffa0fc5e9d8d840d95b9778eb
lightknight64bit <govindsud@gmail.com>
2023-01-08 13:59:40 +0530

    change urdf path

--- launch/urdf.launch.py
+++ launch/urdf.launch.py
@@ -1,9 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 import os
 def generate_launch_description():
-    urdf = '/home/govind/ros_ws/src/urdf_own/urdf/robot.urdf.xacro'
+    urdf = os.path.join(get_package_share_directory("urdf_own"), "urdf/robot.urdf.xacro")
+    
     urdf_ = os.popen(f'xacro {urdf}').read()
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     return LaunchDescription([
